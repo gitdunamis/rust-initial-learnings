5612f0777e43799014b3b5a9db218b7927574541
victor <odebode@getrova.com>
2024-07-20 23:41:25 +0100

    Rc<T> counting refereces

--- list/src/main.rs
+++ list/src/main.rs
@@ -7,4 +7,6 @@ fn main() {
     let tree_one = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     let three = Cons(3, Rc::clone(&tree_one));
     let four = Cons(4, Rc::clone(&tree_one));
+
+    println!("Number of borrows to original data: {}", Rc::strong_count(&tree_one))
 }
