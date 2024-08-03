a7559b3afb8be3171b5d7c35bb04e1ec687c27cc
victor <odebode@getrova.com>
2024-08-03 16:57:16 +0100

    using types to achieve same result as dynamic dispatch

--- traitobjects/src/lib.rs
+++ traitobjects/src/lib.rs
@@ -1,79 +1,38 @@
 
 pub struct Post{
     contents: String,
-    state: Option<Box<dyn State>>
 }
 
 impl Post {
-    pub fn new() -> Self {
-        Post{contents: String::new(), state: Some(Box::new(Draft{}))}
+    pub fn new() -> DraftPost {
+        DraftPost{content: String::new()}
     }
     
     pub fn contents(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
+        &self.contents
     }
-    
+}
+
+pub struct DraftPost {
+    content: String
+}
+
+impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
-        self.contents.push_str(text)
+        self.content.push_str(text)
     }
-    
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
-    }
-    
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
+
+    pub fn request_review(self) -> PendingReview {
+        PendingReview { content: self.content }
     }
 }
 
-trait State {
-    fn approve(self: Box<Self>) -> Box<dyn State>;
-    fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn content<'a>(&self, p: &'a Post) -> &'a str {
-        ""
-    }
+pub struct PendingReview {
+    content: String
 }
-
-struct Draft {}
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {})
+impl PendingReview {
+    pub fn approve(self) -> Post {
+        Post {contents: self.content}
     }
 
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    
-}
-
-struct PendingReview {}
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published{})
-    }
-    
-}
-
-struct Published {}
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-
-    fn content<'a>(&self, p: &'a Post) -> &'a str {
-        &p.contents
-    }
-    
 }
--- traitobjects/src/main.rs
+++ traitobjects/src/main.rs
@@ -1,15 +1,12 @@
 use traitobjects::Post;
 
 fn main() {
-    let mut post: Post = Post::new(); //draft state
-    
+    let mut post = Post::new(); //draft state
     post.add_text("This is the first post here!!!");
-    assert_eq!("", post.contents());
     
-    post.request_review();
-    assert_eq!("", post.contents());
+    let post = post.request_review();
     
-    post.approve();
+    let post = post.approve();
     assert_eq!("This is the first post here!!!", post.contents());
     
 }
